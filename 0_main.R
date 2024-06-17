7ece228c919856c89db86b4107458a0e14be208b
dcarver1 <carver.dan1@gmail.com>
2024-06-17 15:59:32 -0700

    updated the git ignore and mainprocessingscript

---
+++ 0_main.R
@@ -0,0 +1,65 @@
+###
+# Primary processing script for the data processing code base 
+# script is used to established the working environment and call other processing scripts 
+# 202406017 
+# carverd@colostate.edu
+###
+
+
+# source libraries ---
+pacman::p_load(terra,dplyr, stringr, sf, targets, tidycensus,
+               tigris, tmap)
+tmap_mode("view")
+## alt ; install you census api key if you want or provide as parameter in functions 
+# tidycensus::census_api_key(key = "your key",
+#                            install = TRUE,
+#                            overwrite = TRUE)
+
+# source helper function ---
+source("utilities/loadFunctions.R")
+
+# load local utilities ----------------------------------------------------
+loadFunctions("utilities")
+## generate folder structure 
+createFolderStructures()
+
+# load local functions ----
+loadFunctions("functions")
+
+# load local scripts ---- 
+## not sure if want these written as functions or as scripts directly
+## I think I'll lean toward functions for a bit more control of the variables... might need to rename
+loadFunctions("scripts")
+
+
+# pull any external data sets ---- 
+## this should only happen once 
+pullCensusGeographies(overwrite = TRUE)
+
+# set up environment ----
+
+## set spatial scale 
+
+## 
+
+
+
+# Start the targets workflow processing steps ----
+## the processing functions themselves need to work exclusive of the targets frame
+## work. Targets is being used here to ensure efficency in the running of the processing code
+## work on what the best integration between the development stratigies is 
+
+## Environmental Exposures ----
+
+## environmental Effects ---- 
+
+## climate vulnerability ----
+
+## sensitive populations ----
+
+## demographics score ----
+
+
+
+
+
