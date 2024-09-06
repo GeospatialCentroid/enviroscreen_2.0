b966828e33202f5caa00ce56fe930d8fa4e172d7
dcarver1 <carver.dan1@gmail.com>
2024-09-06 15:47:09 -0700

    updated the datasource from rani

---
+++ temp/pullDataFromGoogleDrive.R
@@ -0,0 +1,9 @@
+library(googledrive)
+# pull in the data
+googledrive::drive_download(file = googledrive::as_id("https://drive.google.com/file/d/1DNbW6cX82oBpl9YfiAHV9KS50_XYOPgB/view?usp=drive_link"),
+                            path = "temp/datasets0905")
+# unzip it 
+zipF<-file.choose() # lets you choose a file and save its file path in R (at least for windows)
+outDir<- "temp"# Define the folder where the zip file should be unzipped to 
+unzip(zipF,exdir=outDir)  # unzip your file 
+      
